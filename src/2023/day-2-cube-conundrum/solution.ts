8f3455ef1435bd315683f16b207f3fbcde231623
Artem Prokop <artem13.prokop@gmail.com>
2023-12-02 06:37:18 +0100

    Add 2023 day 2 tasks

---
+++ src/2023/day-2-cube-conundrum/solution.ts
@@ -0,0 +1,69 @@
+import { readInput } from '../../utils.js'
+
+function extractColor(pack: string, color: 'red' | 'green' | 'blue') {
+  const data = pack.match(new RegExp(`(\\d+) ${color}`))
+  if (!data || !data[1]) {
+    return 0
+  }
+  return Number(data[1])
+}
+
+function getGameIdValue(line: string) {
+  const id = Number(line.split(' ')[1].split(':')[0])
+  for (const pack of line.split(';')) {
+    const red = extractColor(pack, 'red')
+    const green = extractColor(pack, 'green')
+    const blue = extractColor(pack, 'blue')
+    if (red > 12 || green > 13 || blue > 14) {
+      return 0
+    }
+  }
+  return id
+}
+
+function getGameCubesValue(line: string) {
+  let minRed = 0
+  let minGreen = 0
+  let minBlue = 0
+  for (const pack of line.split(';')) {
+    const red = extractColor(pack, 'red')
+    minRed = red > minRed ? red : minRed
+    const green = extractColor(pack, 'green')
+    minGreen = green > minGreen ? green : minGreen
+    const blue = extractColor(pack, 'blue')
+    minBlue = blue > minBlue ? blue : minBlue
+  }
+  return minRed * minGreen * minBlue
+}
+
+export async function possibleGames(extended = false) {
+  const input = await readInput(import.meta.url)
+
+  console.log('===============================')
+  console.log('Resolving Cube Possibilities...')
+  console.log('===============================')
+  console.log(`   +--------+
+  /        /|
+ /        / |
++--------+  |   +----+
+|        |  |  /    /|
+|        |  + +----+ |
+|        | /  |    | +
+|        |/   |    |/
++--------+    +----+`)
+  console.log()
+
+  const sum = input
+    .split('\n')
+    .reduce(
+      (sum, line) =>
+        sum + (extended ? getGameCubesValue : getGameIdValue)(line),
+      0
+    )
+
+  console.log('===========================')
+  console.log('Cube Possibilities Resolved')
+  console.log('===========================')
+  console.log()
+  console.log(`Game values sum is ${sum}`)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as readline from 'readline'
 import { trebuchetCalibration } from './2023/day-1-trebuchet/solution.js'
+import { possibleGames } from './2023/day-2-cube-conundrum/solution.js'
 
 console.log('===========================')
 console.log("ExposedCat's Advent of Code")
@@ -11,7 +12,8 @@ const reader = readline.createInterface({
   output: process.stdout
 })
 
-const solutions = '- Day 1: Trebuchet?! (tasks 1, 2)'
+const solutions = `- Day 1: Trebuchet?! (tasks 1, 2)
+- Day 2: Cube Conundrum (tasks 1, 2)`
 const question = 'Enter a problem in the format `day.task` = '
 
 reader.question(`${solutions}\n\n${question}`, answer => {
@@ -20,6 +22,10 @@ reader.question(`${solutions}\n\n${question}`, answer => {
   switch (day) {
     case '1': {
       trebuchetCalibration(task === '2')
+      break
+    }
+    case '2': {
+      possibleGames(task === '2')
       break
     }
     default: {
